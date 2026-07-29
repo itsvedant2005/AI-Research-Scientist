2745867e0943ce8d896b61ca7c820ba2276582bb
itsvedant2005 <vedantcwankhede1005@gmail.com>
2026-07-30 04:01:40 +0530

    Configure frontend environment variables

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-         "https://ai-research-frontend.onrender.com"
+         "https://ai-research-backend-lala.onrender.com/"
         # Replace with your frontend URL after deployment
         # "https://your-frontend.onrender.com",
     ],
